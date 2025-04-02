b7f71a7d2dd06d3047628ec59bc56169fbc93b1f
aradcharon <aradshafiee06@gmail.com>
2025-04-02 11:28:47 +0330

    one step

---
+++ src/db/Database.java
@@ -0,0 +1,37 @@
+package db;
+
+import db.exeption.EntityNotFoundException;
+
+import java.util.ArrayList;
+
+public class Database {
+    private static final ArrayList<Entity> entities = new ArrayList<>();
+    private static int nextId = 1;
+
+    private Database() {}
+
+    public static void add(Entity e) {
+        e.id = nextId++;
+        entities.add(e);
+    }
+
+    public static Entity get(int id) throws EntityNotFoundException {
+        for (Entity e : entities) {
+            if (e.id == id) {
+                return e;
+            }
+        }
+        throw new EntityNotFoundException(id);
+    }
+
+    public static void delete(int id) throws EntityNotFoundException {
+        Entity e = get(id);
+        entities.remove(e);
+    }
+
+    public static void update(Entity e) throws EntityNotFoundException {
+        Entity existing = get(e.id);
+        int index = entities.indexOf(existing);
+        entities.set(index, e);
+    }
+}
