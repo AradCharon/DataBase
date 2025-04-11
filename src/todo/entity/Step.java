8b745dee4697bf75e82f45b201c68bad89ca3a5d
aradcharon <aradshafiee06@gmail.com>
2025-04-11 21:59:43 +0330

    add Step class

---
+++ src/todo/entity/Step.java
@@ -0,0 +1,27 @@
+package todo.entity;
+
+import db.Entity;
+import java.util.Date;
+
+public class Step extends Entity {
+    public enum Status { NotStarted, Completed }
+
+    public String title;
+    public Status status;
+    public int taskRef;
+
+    @Override
+    public Step copy() {
+        Step copy = new Step();
+        copy.id = this.id;
+        copy.title = this.title;
+        copy.status = this.status;
+        copy.taskRef = this.taskRef;
+        return copy;
+    }
+
+    @Override
+    public int getEntityCode() {
+        return 2;
+    }
+}
