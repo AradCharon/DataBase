a06d69171c6aecd20d830fc6906eaa2bdcae6ce1
aradcharon <aradshafiee06@gmail.com>
2025-04-11 21:51:02 +0330

    first five

--- src/todo/entity/Task.java
+++ src/todo/entity/Task.java
@@ -1,4 +1,55 @@
 package todo.entity;
 
-public class Task {
+import db.Entity;
+import db.Trackable;
+import java.util.Date;
+
+public class Task extends Entity implements Trackable {
+    public enum Status { NotStarted, InProgress, Completed }
+
+    private Date creationDate;
+    private Date lastModificationDate;
+    public String title;
+    public String description;
+    public Date dueDate;
+    public Status status;
+    
+    @Override
+    public Task copy() {
+        Task copy = new Task();
+        copy.id = this.id;
+        copy.title = this.title;
+        copy.description = this.description;
+        copy.dueDate = this.dueDate != null ? new Date(this.dueDate.getTime()) : null;
+        copy.status = this.status;
+        copy.creationDate = this.creationDate != null ? new Date(this.creationDate.getTime()) : null;
+        copy.lastModificationDate = this.lastModificationDate != null ? new Date(this.lastModificationDate.getTime()) : null;
+        return copy;
+    }
+
+    @Override
+    public int getEntityCode() {
+        return 1;
+    }
+
+    // Trackable interface implementation
+    @Override
+    public void setCreationDate(Date date) {
+        this.creationDate = date;
+    }
+
+    @Override
+    public Date getCreationDate() {
+        return creationDate;
+    }
+
+    @Override
+    public void setLastModificationDate(Date date) {
+        this.lastModificationDate = date;
+    }
+
+    @Override
+    public Date getLastModificationDate() {
+        return lastModificationDate;
+    }
 }
