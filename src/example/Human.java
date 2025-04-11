e5369048fbf31db9731bc32204b707631001a788
aradcharon <aradshafiee06@gmail.com>
2025-04-11 15:16:14 +0330

    human class chenge

--- src/example/Human.java
+++ src/example/Human.java
@@ -3,16 +3,22 @@ package example;
 import db.Entity;
 
 public class Human extends Entity {
+    public static final int HUMAN_ENTITY_CODE = 14;
     public String name;
+    public int age;
 
-    public Human(String name) {
+    public Human(String name, int age) {
         this.name = name;
+        this.age = age;
+    }
+
+    public Human(Human human) {
+        this.name = human.name;
+        this.age = human.age;
     }
 
     @Override
-    public Human copy() {
-        Human copy = new Human(this.name);
-        copy.id = this.id;
-        return copy;
+    public int getEntityCode() {
+        return HUMAN_ENTITY_CODE;
     }
 }
